c1060f1e6f3ce72be6df803ab15af252d24880df
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-11-15 22:50:10 -0800

    connection to db.js fixed

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,4 @@
+
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
@@ -7,22 +8,19 @@ const FILES_TO_CACHE = [
     '/manifest.webmanifest',
     '/styles.css',
     '/index.js',
+    "/db.js",
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
 ];
 
 // install
 self.addEventListener("install", evt => {
-    // pre cache image data
-    evt.waitUntil(
-        caches.open(DATA_CACHE_NAME)
-        .then((cache) => cache.add("/api/transaction")) 
-    );
-
      // pre cache all static assets
     evt.waitUntil(
-        caches.open(CACHE_NAME).
-        then((cache) => cache.addAll(FILES_TO_CACHE))
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE)
+        })
     );
 
     // tell the browser to activate this service worker immediately once it has finished installing
